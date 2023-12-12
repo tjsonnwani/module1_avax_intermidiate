832fbbd315a7104c870f7da26e84030ca8c9edd5
tjsonnwani <148685614+tjsonnwani@users.noreply.github.com>
2023-12-12 18:28:28 +0530

    Update module1.sol

--- module1.sol
+++ module1.sol
@@ -2,23 +2,23 @@
 pragma solidity ^0.8.0;
 
 contract myContract {
-    uint256 public mymoney ;
+    uint256 public myamt ;
 
-    function setmymoney(uint256 newmoney) public {
+    function setmymoney(uint256 newamt) public {
         // Using require() to validate a condition
         //if this condition true the 
-        require(newmoney > 100, "Value must be greater than 100");
+        require(newamt > 100, "Value must be greater than 100");
 
         // Using assert() to ensure an internal condition
         //and to check the invariants
-        assert(newmoney < 1000);
+        assert(newamt < 1000);
 
         // Setting the value
-        if(newmoney == 250){
+        if(newamt == 250){
             // Using revert() to revert the transaction
         revert("This operation is not allowed"); 
         }
-        mymoney = newmoney;
+        myamt = newamt;
         
         
     }
